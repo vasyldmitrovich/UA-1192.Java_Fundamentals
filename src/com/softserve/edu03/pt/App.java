5dac8361b4dfa86b1322bdaafd3ae8564c126012
Vasyldmitrovich <vasyl.dmitrovich@gmail.com>
2024-04-26 20:54:34 +0300

    edu 03 hw1

---
+++ src/com/softserve/edu03/pt/App.java
@@ -0,0 +1,9 @@
+package com.softserve.edu03.pt;
+
+import java.util.Arrays;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("getTotal result is: " + Task1.getTotal(5, 7));
+    }
+}
---
+++ src/com/softserve/edu03/pt/Task1.java
@@ -0,0 +1,8 @@
+package com.softserve.edu03.pt;
+
+public class Task1 {
+    public static int getTotal(int first, int second) {
+        //int result = first + second;
+        return first + second;
+    }
+}
