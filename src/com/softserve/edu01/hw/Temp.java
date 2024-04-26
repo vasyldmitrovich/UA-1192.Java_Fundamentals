2697c68e44da078ab9e5830da1be9b57972dbfb8
Vasyldmitrovich <vasyl.dmitrovich@gmail.com>
2024-04-26 20:24:22 +0300

    Del Temp.java

--- src/com/softserve/edu01/hw/Temp.java
+++
@@ -1,9 +0,0 @@
-package com.softserve.edu01.hw;
-
-import java.util.Arrays;
-
-public class Temp {
-    public static void main(String[] args) {
-        System.out.println("Temp class = ");
-    }
-}
