a5ceb6244d363bb54595d5653e5ffe5948729863
SaenkoVova <vova080520@gmail.com>
2024-04-26 18:50:01 +0300

    Add edu01 tasks

--- src/com/softserve/edu01/hw/App.java
+++ src/com/softserve/edu01/hw/App.java
@@ -1,34 +1,64 @@
 package com.softserve.edu01.hw;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App {
-    public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        App.task1();
+        task1();
         task2();
+        task3();
     }
 
     public static void task1() {
-        System.out.println("Hello. What is your name?");
-        String name = scanner.nextLine();
-        System.out.println("How old are you?");
-        String age = scanner.nextLine();
-        System.out.println("Hello " + name);
-        System.out.println("You are " + age);
+        try {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Specify the radius of the circle: ");
+            double radius = in.nextDouble();
+
+            double perimeter = 2 * Math.PI * radius;
+            double square = Math.PI * Math.pow(radius, 2);
+
+            System.out.println("The perimeter of the circle is equal: " + perimeter);
+            System.out.println("The square of the circle is equal: " + square);
+        } catch (InputMismatchException inputMismatchException) {
+            System.out.println("Radius should by a number!");
+        }
     }
 
     public static void task2() {
-        System.out.println("Input first string");
-        String first = scanner.nextLine();
-        System.out.println("Input second string");
-        String second = scanner.nextLine();
-        System.out.println("Input third string");
-        String third = scanner.nextLine();
-        System.out.println(third);
-        System.out.println(second);
-        System.out.println(first);
+        String name;
+        String address;
+        Scanner in = new Scanner(System.in);
+        System.out.println("What is your name?");
+        name = in.nextLine();
+        System.out.println("Where do you live, " + name + "?");
+        address = in.nextLine();
+        System.out.println("Your name is " + name + ", your address is " + address);
+    }
+
+    public static void task3() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Specify the cost per minute of the first call: ");
+        double costPerMin1 = in.nextDouble();
+        System.out.println("Specify the duration of the first call in minutes: ");
+        double duration1 = in.nextDouble();
+        System.out.println("Specify the cost per minute of the second call: ");
+        double costPerMin2 = in.nextDouble();
+        System.out.println("Specify the duration of the second call in minutes: ");
+        double duration2 = in.nextDouble();
+        System.out.println("Specify the cost per minute of the third call in minutes: ");
+        double costPerMin3 = in.nextDouble();
+        System.out.println("Specify the duration of the third call: ");
+        double duration3 = in.nextDouble();
+        double cost1 = costPerMin1 * duration1;
+        double cost2 = costPerMin2 * duration2;
+        double cost3 = costPerMin3 * duration3;
+        System.out.println("The cost of the first call is equal: " + cost1);
+        System.out.println("The cost of the second call is equal: " + cost2);
+        System.out.println("The cost of the third call is equal: " + cost3);
+        System.out.println("The total cost is equal: " + (cost1 + cost2 + cost3));
     }
 }
 
--- src/com/softserve/edu01/pt/App.java
+++ src/com/softserve/edu01/pt/App.java
@@ -1,15 +1,67 @@
 package com.softserve.edu01.pt;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        task1();
+        task2();
+        task3();
+        task4();
+    }
 
-        // Practical tasks
+    public static void task1() throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Input text: ");
+        String firstString = bufferedReader.readLine();
+        String secondString = bufferedReader.readLine();
+        String thirdString = bufferedReader.readLine();
+        System.out.println(thirdString);
+        System.out.println(secondString);
+        System.out.println(firstString);
+    }
 
-        // Task 1
+    public static void task2() {
+        try {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Enter the first number: ");
+            double firstNumber = in.nextDouble();
+            System.out.println("Enter the second number: ");
+            double secondNumber = in.nextDouble();
+            System.out.println("Enter the third number: ");
+            double thirdNumber = in.nextDouble();
+            double avg = (firstNumber + secondNumber + thirdNumber) / 3;
+            System.out.println("The average is " + avg);
+        } catch (InputMismatchException inputMismatchException) {
+            System.out.println("You need to enter a number!");
+        }
+    }
 
-        // Task 2
+    public static void task3() {
+        try {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Enter a: ");
+            int a = in.nextInt();
+            System.out.println("Enter b: ");
+            int b = in.nextInt();
 
-        // Task 3...
+            System.out.println("a + b = " + (a + b));
+            System.out.println("a - b = " + (a - b));
+            System.out.println("a * b = " + (a * b));
+            System.out.println("a / b = " + ((double)a / b));
+        } catch (InputMismatchException inputMismatchException) {
+            System.out.println("You need to enter an integer number!");
+        }
+    }
 
+    public static void task4() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("How are you?");
+        String answer = in.nextLine();
+        System.out.println("You are (" + answer + ")");
     }
 }
