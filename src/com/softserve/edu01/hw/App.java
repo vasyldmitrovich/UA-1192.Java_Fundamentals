8a12ffc0d4e027232ac6927a5cf7290296b92cd1
OleksandrKhomenko <o.o.khomenko.vntu@gmail.com>
2024-04-26 22:14:47 +0300

    Implemented practical tasks and homework

--- src/com/softserve/edu01/hw/App.java
+++ src/com/softserve/edu01/hw/App.java
@@ -6,30 +6,46 @@ public class App {
     public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        App.task1();
-        task2();
-//        task3();
+        circle();
+        conversation();
+        phoneCalls();
     }
 
-    public static void task1() {
-        System.out.println("Hello. What is your name?");
+    public static void circle() {
+        System.out.println("Type radius");
+        double radius = scanner.nextDouble();
+        double area = Math.PI * Math.pow(radius, 2);
+        double perimetr = 2 * Math.PI * radius;
+        System.out.println("area is: " + area + "\nperimetr is: " + perimetr);
+    }
+
+    public static void conversation() {
+        scanner.nextLine();
+        System.out.println("What is your name?");
         String name = scanner.nextLine();
-        System.out.println("How old are you?");
-        String age = scanner.nextLine();
-        System.out.println("Hello " + name);
-        System.out.println("You are " + age);
+        System.out.println("Where do you live " + name + "?");
+        String address = scanner.nextLine();
+        System.out.println("Now I know where you live " + name + " you live in " + address);
     }
 
-    public static void task2() {
-        System.out.println("Input first string");
-        String first = scanner.nextLine();
-        System.out.println("Input second string");
-        String second = scanner.nextLine();
-        System.out.println("Input third string");
-        String third = scanner.nextLine();
-        System.out.println(third);
-        System.out.println(second);
-        System.out.println(first);
+    public static void phoneCalls() {
+        double total = 0;
+        double[] c = new double[3];
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Input "+(i+1)+" call cost per minute");
+            c[i] = scanner.nextDouble();
+        }
+        double[] t = new double[3];
+        for (int k = 0; k < 3; k++) {
+            System.out.println("Input "+(k+1)+" calls duration");
+            t[k] = scanner.nextDouble();
+        }
+        for (int j = 0; j < 3; j++) {
+            System.out.println("Cost of " + (j + 1) + " call is " + c[j] * t[j]);
+            total = total + c[j] * t[j];
+        }
+        System.out.println("Total cost is " + total);
     }
+
 }
 
--- src/com/softserve/edu01/pt/App.java
+++ src/com/softserve/edu01/pt/App.java
@@ -1,15 +1,53 @@
 package com.softserve.edu01.pt;
 
+import java.util.Scanner;
+
 public class App {
+    public static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-
-        // Practical tasks
-
-        // Task 1
-
-        // Task 2
-
-        // Task 3...
-
+        firstTask();
+        averageNumber();
+        calculator();
+        dialogue();
     }
+
+    private static void firstTask() {
+        System.out.println("Input 3 lines of text:");
+        String Line1 = scanner.nextLine();
+        String Line2 = scanner.nextLine();
+        String Line3 = scanner.nextLine();
+        System.out.printf("\nOutput text: \n" + Line3 + "\n" + Line2 + "\n" + Line1 + "\n");
+    }
+
+    public static void averageNumber() {
+        System.out.println("Input first number:");
+        int firstNum = scanner.nextInt();
+        System.out.println("Input second number:");
+        int secondtNum = scanner.nextInt();
+        System.out.println("Input third number:");
+        int thirdtNum = scanner.nextInt();
+        double average = (firstNum + secondtNum + thirdtNum) / 3.0;
+        System.out.println("Average is " + average + "\n");
+    }
+
+    public static void calculator() {
+        System.out.println("Input number a:");
+        int a = scanner.nextInt();
+        System.out.println("Input number b:");
+        int b = scanner.nextInt();
+        int sum = a + b;
+        int diff = a - b;
+        int mul = a * b;
+        double division = (double) a / (double) b;
+        System.out.printf("a+b =" + sum + "\na-b =" + diff + "\na*b =" + mul + "\na/b =" + division + "\n");
+    }
+
+    public static void dialogue() {
+        System.out.println("How are you?");
+        scanner.nextLine();
+        String answer = scanner.nextLine();
+        System.out.println("You are " + answer + "\n");
+    }
+
 }
