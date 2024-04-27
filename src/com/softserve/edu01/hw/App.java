25b50123a871f386f01edd6ad1a546e524ae13ac
Rostyslav Zadyraichuk <rostykzadyraichuk010@gmail.com>
2024-04-27 14:13:32 +0300

    Add homework 01-intro

--- src/com/softserve/edu01/hw/App.java
+++ src/com/softserve/edu01/hw/App.java
@@ -1,34 +1,69 @@
 package com.softserve.edu01.hw;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class App {
-    public static Scanner scanner = new Scanner(System.in);
+
+    public static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        App.task1();
-        task2();
+        try {
+            doFlowerTask();
+            doNameAddressTask();
+            doPhoneCallTask();
+        } catch (IOException ignored) {}
     }
 
-    public static void task1() {
-        System.out.println("Hello. What is your name?");
-        String name = scanner.nextLine();
-        System.out.println("How old are you?");
-        String age = scanner.nextLine();
-        System.out.println("Hello " + name);
-        System.out.println("You are " + age);
+    public static void doFlowerTask() throws IOException {
+        System.out.println("--- Flower bed task ---");
+        System.out.print("Input flower bed radius in meters: ");
+        double radiusInMeters = sc.nextDouble();
+        double perimeter = 2 * Math.PI * radiusInMeters;
+        //double area = Math.PI * radiusInMeters * radiusInMeters;
+        double area = Math.PI * Math.pow(radiusInMeters, 2);
+        System.out.println("Flower bed perimeter: " + perimeter + " m");
+        System.out.println("Flower bed area: " + area + " m2");
     }
 
-    public static void task2() {
-        System.out.println("Input first string");
-        String first = scanner.nextLine();
-        System.out.println("Input second string");
-        String second = scanner.nextLine();
-        System.out.println("Input third string");
-        String third = scanner.nextLine();
-        System.out.println(third);
-        System.out.println(second);
-        System.out.println(first);
+    public static void doNameAddressTask() throws IOException {
+        System.out.println("\n--- Name & Address task ---");
+        String name;
+        String address;
+        System.out.println("What is your name?");
+        name = sc.nextLine();
+        System.out.println("Where do you live, " + name + '?');
+        address = sc.nextLine();
+        System.out.println("Nice to meet you, " + name + ". I live in " + address + " too.");
+    }
+
+    public static void doPhoneCallTask() throws IOException {
+        System.out.println("\n--- Phone calls task ---");
+        System.out.print("Input first call's cost per minute: ");
+        double c1 = sc.nextDouble();
+        System.out.print("Input second call's cost per minute: ");
+        double c2 = sc.nextDouble();
+        System.out.print("Input third call's cost per minute: ");
+        double c3 = sc.nextDouble();
+
+        System.out.print("Input first call's duration in seconds: ");
+        int t1 = sc.nextInt();
+        System.out.print("Input second call's duration in seconds: ");
+        int t2 = sc.nextInt();
+        System.out.print("Input third call's duration in seconds: ");
+        int t3 = sc.nextInt();
+
+        double firstCallCost = (t1 / 60) * c1; //cost for full minutes
+        firstCallCost += (t1 % 60) * c1 / 60;   //add cost for seconds that remain
+        double secondCallCost = (t2 / 60) * c2;
+        secondCallCost += (t2 % 60) * c2 / 60;
+        double thirdCallCost = (t3 / 60) * c3;
+        thirdCallCost += (t3 % 60) * c3 / 60;
+
+        System.out.println("First call cost: " + firstCallCost);
+        System.out.println("Second call cost: " + secondCallCost);
+        System.out.println("Third call cost: " + thirdCallCost);
+        System.out.println("Total cost: " + (firstCallCost + secondCallCost + thirdCallCost));
     }
 }
 
