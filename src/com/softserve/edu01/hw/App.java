17007ec50435647df1ef68983694f99ea74e058d
RostyslavKushpit <rostyslavkushpit@gmail.com>
2024-04-27 19:47:41 +0300

    Added hw1

--- src/com/softserve/edu01/hw/App.java
+++ src/com/softserve/edu01/hw/App.java
@@ -1,34 +1,64 @@
 package com.softserve.edu01.hw;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class App {
-    public static Scanner scanner = new Scanner(System.in);
+        public static void main(String[] args) throws IOException  {
+                App.exercise1();
+                App.exercise2();
+                App.exercise3();
 
-    public static void main(String[] args) {
-        App.task1();
-        task2();
-    }
+        }
+        public static void exercise1() throws IOException {
+                BufferedReader exercise1 = new BufferedReader(new InputStreamReader(System.in));
+                System.out.println("Flower bed is shaped like a circle.Let's Calculate its perimeter and area!\nInput the radius: ");
+                Double r = Double.parseDouble(exercise1.readLine());
+                Double p = 3.14;
+                Double perimetr = 2*p*r;
+                Double area = p*(r*r);
+                System.out.println("Perimetr of flower bed is " + perimetr);
+                System.out.println("Area of flower bed is " + area);
 
-    public static void task1() {
-        System.out.println("Hello. What is your name?");
-        String name = scanner.nextLine();
-        System.out.println("How old are you?");
-        String age = scanner.nextLine();
-        System.out.println("Hello " + name);
-        System.out.println("You are " + age);
-    }
+        }
 
-    public static void task2() {
-        System.out.println("Input first string");
-        String first = scanner.nextLine();
-        System.out.println("Input second string");
-        String second = scanner.nextLine();
-        System.out.println("Input third string");
-        String third = scanner.nextLine();
-        System.out.println(third);
-        System.out.println(second);
-        System.out.println(first);
-    }
+        public static void exercise2() throws IOException {
+                BufferedReader exercise2 = new BufferedReader(new InputStreamReader(System.in));
+                System.out.println("\nWhat is your name?");
+                String name = exercise2.readLine();
+                System.out.println("\nWhere do you live, " + name+ "?");
+                String adress = exercise2.readLine();
+                System.out.println("\nOkay, your name is "+name + " and you are living in " + adress);
+        }
+        public static void exercise3() throws IOException {
+                BufferedReader exercise3 = new BufferedReader(new InputStreamReader(System.in));
+                System.out.println("\nSpecify the cost of the first call: ");
+                Double c1 = Double.parseDouble(exercise3.readLine());
+                System.out.println("\nSpecify the duration of the first call: ");
+                Double t1 = Double.parseDouble(exercise3.readLine());
+                Double c1value = c1*t1;
+
+                System.out.println("\nSpecify the cost of the second call: ");
+                Double c2 = Double.parseDouble(exercise3.readLine());
+                System.out.println("\nSpecify the duration of the second call: ");
+                Double t2 = Double.parseDouble(exercise3.readLine());
+                Double c2value = c2*t2;
+
+                System.out.println("\nSpecify the cost of the third call: ");
+                Double c3 = Double.parseDouble(exercise3.readLine());
+                System.out.println("\nSpecify the duration of the third call: ");
+                Double t3 = Double.parseDouble(exercise3.readLine());
+                Double c3value = c3*t3;
+
+                Double totalsum = c1value + c2value + c3value;
+                System.out.println("\nCost of the first call was: " + c1value);
+                System.out.println("\nCost of the second call was: " + c2value);
+                System.out.println("\nCost of the third call was: " + c3value);
+                System.out.println("\nTotal amount of all calls is: " + totalsum);
+
+        }
+
 }
 
