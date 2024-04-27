435e2c5d369acab0d1b809420208dc120e787a6c
Dmitry Cherkasov <diatrim@gmail.com>
2024-04-27 15:36:26 +0300

    dev01 practical tasks + homework

--- src/com/softserve/edu01/hw/App.java
+++ src/com/softserve/edu01/hw/App.java
@@ -1,34 +1,87 @@
 package com.softserve.edu01.hw;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class App {
-    public static Scanner scanner = new Scanner(System.in);
+    public class App {
+        public static Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        App.task1();
-        task2();
+        public static void main(String[] args) {
+
+            task1();
+            task2();
+            task3();
+
+        }
+
+        public static void task1() {
+/*
+        Flower bed is shaped like a circle. Calculate its perimeter and area. Input the radius
+        from the console, and output obtained results
+ */
+            System.out.println("Please enter value of circle's radius: ");
+            double radius = readInput();
+            double perimeter = 2 * Math.PI * radius;
+            System.out.println("Perimeter of your circle is: " + perimeter);
+            double area = Math.PI * Math.pow(radius, 2.0);
+            System.out.println("Area of your circle is: " + area);
+
+        }
+
+        public static void task2() {
+/*
+        Define two String variables name and address. Output question "What is your name?"
+        to the console. Read the value of name and output next question: “Where do you live,
+        (name)?“ to the console. Read the value of address from the console and output the
+        complete information.
+ */
+            System.out.println("What is your name?");
+            Scanner scanner = new Scanner(System.in);
+            String name = scanner.nextLine();
+            System.out.println("Where do you live, " + name + "?");
+            String address = scanner.nextLine();
+
+            System.out.printf("%s, thanks for your information. Your address is %s%n", name, address);
+
+        }
+
+        public static void task3() {
+/*
+        Three phone calls were made from different countries, each with a different cost per
+        minute (c1, c2, and c3) and duration (t1, t2, and t3). Input all the source data from the
+        console, calculate the cost of each call and the total cost, and output the results to the
+        console.
+ */
+            double[] costs = new double[3];
+            int[] durations = new int[3];
+            for (int i = 0; i < 3; i++) {
+                System.out.println("Enter country tariff: ");
+                costs[i] = readInput() / 60;
+                System.out.println("Enter call duration in seconds: ");
+                durations[i] = scanner.nextInt();
+            }
+            double totalCost = 0;
+            for (int i = 0; i < 3; i++) {
+                int callNumber = i+1;
+                totalCost += costs[i]*durations[i];
+                System.out.println("Call number " + callNumber + " cost is: " + String.format("%.2f", costs[i]*durations[i]));
+            }
+            System.out.println("The total cost is: " + String.format("%.2f", totalCost));
+
+
+        }
+
+
+        private static double readInput() {
+            scanner = new Scanner(System.in);
+            double input;
+            try {
+                input = scanner.nextDouble();
+
+            } catch (InputMismatchException exception) {
+                System.out.println("You should enter the number, please try again: ");
+                return readInput();
+            }
+            return input;
+        }
     }
-
-    public static void task1() {
-        System.out.println("Hello. What is your name?");
-        String name = scanner.nextLine();
-        System.out.println("How old are you?");
-        String age = scanner.nextLine();
-        System.out.println("Hello " + name);
-        System.out.println("You are " + age);
-    }
-
-    public static void task2() {
-        System.out.println("Input first string");
-        String first = scanner.nextLine();
-        System.out.println("Input second string");
-        String second = scanner.nextLine();
-        System.out.println("Input third string");
-        String third = scanner.nextLine();
-        System.out.println(third);
-        System.out.println(second);
-        System.out.println(first);
-    }
-}
-
--- src/com/softserve/edu01/pt/App.java
+++ src/com/softserve/edu01/pt/App.java
@@ -1,15 +1,117 @@
 package com.softserve.edu01.pt;
 
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class App {
+    public static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
+        App.task1();
+        task2();
+        task3();
+        task4();
+    }
 
-        // Practical tasks
+    public static void task1() {
+/*
+        Write a program that reads three strings and outputs them in the reverse order, each
+        on a new line.
+ */
 
-        // Task 1
+        String[] log = new String[3];
+        System.out.println("Print some text: ");
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 2; i >= 0; i--) {
+            log[i] = scanner.nextLine();
+        }
+        System.out.println("Reversed output: ");
+        for (String element : log) {
+            System.out.println(element);
+        }
+    }
 
-        // Task 2
+    public static void task2() {
+/*
+Write a program that takes three numbers from console as input to calculate and print
+output the average of the numbers
+ */
+        double[] arr = new double[3];
+        double sum = 0;
 
-        // Task 3...
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Please enter number: ");
+            arr[i] = readDoubleInput();
+            sum += arr[i];
+        }
+        double avg = sum / arr.length;
+        System.out.println("The average of entered numbers is: " + avg);
+    }
+
+    private static void task3() {
+/*
+        Define integer variables a and b. Read values a and b from Console, perform basic calculations
+        and output results.
+ */
+        System.out.println("Enter variable \"a\" integer value: ");
+        int a = readIntInput();
+        System.out.println("Enter variable \"b\" integer value: ");
+        int b = readIntInput();
+        System.out.println("Calculation results: ");
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + a * b);
+        System.out.println("a / b = " + (double)a / b);
 
     }
+
+    private static void task4() {
+/*
+        Output question “How are you?“. Define string variable answer. Read the value
+        answer and output: “You are (answer)".
+ */
+
+        System.out.println("Hi there!");
+        System.out.println("How are you doing? ");
+        Scanner scanner = new Scanner(System.in);
+        String answer = scanner.nextLine();
+
+        if(answer.toLowerCase(Locale.ROOT).equals("good") || answer.toLowerCase(Locale.ROOT).equals("fine")){
+            System.out.println("I`m glad to hear you are fine)");
+        } else {
+            System.out.println("So you are " + answer);
+        }
+
+    }
+
+
+    private static int readIntInput() {
+        scanner = new Scanner(System.in);
+        int input;
+        try {
+            input = scanner.nextInt();
+
+        } catch (InputMismatchException exception) {
+            System.out.println("You should enter integer number, please try again: ");
+            return readIntInput();
+        }
+        return input;
+    }
+
+    private static double readDoubleInput() {
+        scanner = new Scanner(System.in);
+        double input;
+        try {
+            input = scanner.nextDouble();
+
+        } catch (InputMismatchException exception) {
+            System.out.println("You should enter the number, please try again: ");
+            return readDoubleInput();
+        }
+        return input;
+    }
+
+
 }
