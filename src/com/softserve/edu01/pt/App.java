44f6282f69702ad78c763ae968551a0d91195e28
akvitka <invariant17@gmail.com>
2024-04-27 12:40:35 +0300

    Practical tasks 1,2

--- src/com/softserve/edu01/pt/App.java
+++ src/com/softserve/edu01/pt/App.java
@@ -1,15 +1,89 @@
 package com.softserve.edu01.pt;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String method;
+        do {
+            printReadMe();
+            try {
+                method = reader.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            switch (method) {
+                //pt1
+                case "1":
+                    readAndReverse(reader);
+                    break;
+                //pt2
+                case "2":
+                    calculateAverage(reader);
+                    break;
+                //pt3
+                case "3":
+                    calculateOperation();
+                    break;
+                //pt4
+                case "4":
+                    askAndAnswerTheQuestion();
+                    break;
+                default:
+                    System.out.println("Wrong number, try again :)");
+                    break;
+            }
+        } while (true);
+    }
 
-        // Practical tasks
+    private static void printReadMe() {
+        System.out.println("Chose you method");
+        System.out.println("1 - for read and reverse String");
+        System.out.println("2 - for calculate average");
+        System.out.println("3 - for calculate operation");
+        System.out.println("4 - for ask and answer a question");
+    }
 
-        // Task 1
+    private static void readAndReverse(BufferedReader reader) {
+        System.out.println("Write your String for reverse");
+        try {
+            String[] originalString = reader.readLine().split(" ");
+            System.out.println("Your reversed String");
+            Arrays.stream(originalString).toList()
+                    .reversed()
+                    .forEach(s -> {
+                        System.out.print(s + " ");
+                    });
+            System.out.println();
+        } catch (IOException e) {
+            System.out.println("Something went wrong...");
+        }
+    }
 
-        // Task 2
+    private static void calculateAverage(BufferedReader reader) {
+        int a, b, c = 0;
+        try {
+            System.out.println("Enter first number");
+            a = Integer.parseInt(reader.readLine());
+            System.out.println("Enter second number");
+            b = Integer.parseInt(reader.readLine());
+            System.out.println("Enter third number");
+            c = Integer.parseInt(reader.readLine());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Average : " +(a + b + c) / 3);
+    }
 
-        // Task 3...
+    private static void calculateOperation() {
+
+    }
+
+    private static void askAndAnswerTheQuestion() {
 
     }
 }
