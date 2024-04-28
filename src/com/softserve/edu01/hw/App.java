5926ca352807d4b9d6309a4d39b2eae021b23525
Дмитро <deputatfag@gmail.com>
2024-04-28 13:21:10 +0300

    homework1

--- src/com/softserve/edu01/hw/App.java
+++ src/com/softserve/edu01/hw/App.java
@@ -41,8 +41,6 @@ public class App {
         double num3 = scanner.nextDouble();
         double average = (num1 + num2 + num3) / 3;
         System.out.println("The average of the three numbers is: " + average);
-
-
     }
 }
 
