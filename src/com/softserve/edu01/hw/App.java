2f6b12cbd7bf1a20afeb8338bd7b9799ccf5edb8
ViktoriiaStetsenko <stetsenko.viktoriai@gmail.com>
2024-04-28 13:45:19 +0300

    HW1 & PT1

--- src/com/softserve/edu01/hw/App.java
+++ src/com/softserve/edu01/hw/App.java
@@ -1,34 +1,76 @@
 package com.softserve.edu01.hw;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class App {
-    public static Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args) {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
         App.task1();
         task2();
+        task3();
     }
 
-    public static void task1() {
-        System.out.println("Hello. What is your name?");
-        String name = scanner.nextLine();
-        System.out.println("How old are you?");
-        String age = scanner.nextLine();
-        System.out.println("Hello " + name);
-        System.out.println("You are " + age);
+    private static void task1() throws IOException {
+        System.out.println("Enter the radius");
+
+        double radius = Double.parseDouble (br.readLine());
+        double perimeter = (2 * Math.PI * radius);
+        double area = (Math.PI * Math.pow(radius,2));
+
+        System.out.println("Perimeter = " + perimeter);
+        System.out.println("Area = " + area);
     }
 
-    public static void task2() {
-        System.out.println("Input first string");
-        String first = scanner.nextLine();
-        System.out.println("Input second string");
-        String second = scanner.nextLine();
-        System.out.println("Input third string");
-        String third = scanner.nextLine();
-        System.out.println(third);
-        System.out.println(second);
-        System.out.println(first);
+    private static void task2() throws IOException {
+        System.out.println("What is your name?");
+
+        String name = br.readLine();
+        System.out.println("Where do you live, " + name + "?");
+
+        String address = br.readLine();
+        System.out.println(name + " lives in " + address);
     }
-}
+
+    private static void task3() throws IOException {
+        System.out.println("Enter the cost per minute of the first call ($)" );
+        double c1 = Double.parseDouble(br.readLine());
+
+        System.out.println("Enter the duration of the first call (min)" );
+        double t1 = Double.parseDouble(br.readLine());
+
+        System.out.println("Enter the cost per minute of the second call ($)" );
+        double c2 = Double.parseDouble(br.readLine());
+
+        System.out.println("Enter the duration of the second call (min)" );
+        double t2 = Double.parseDouble(br.readLine());
+
+        System.out.println("Enter the cost per minute of the third call ($)" );
+        double c3 = Double.parseDouble(br.readLine());
+
+        System.out.println("Enter the duration of the third call (min)" );
+        double t3 = Double.parseDouble(br.readLine());
+
+        double costFirstCall = c1 * t1;
+        double costSecondCall = c2 * t2;
+        double costThirdCall = c3 * t3;
+        double totalCost = costFirstCall + costSecondCall + costThirdCall;
+
+        System.out.println("The cost of the first call = " + costFirstCall + "$");
+        System.out.println("The cost of the second call = " + costSecondCall + "$");
+        System.out.println("The cost of the third call = " + costThirdCall + "$");
+        System.out.println("The total cost = " + totalCost + "$");
+    }
+    }
+
+
+
+
+
+
+
+
+
+
 
--- src/com/softserve/edu01/pt/App.java
+++ src/com/softserve/edu01/pt/App.java
@@ -1,15 +1,76 @@
 package com.softserve.edu01.pt;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class App {
-    public static void main(String[] args) {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        // Practical tasks
+    public App() throws IOException {
+    }
 
-        // Task 1
+    public static void main(String[] args) throws IOException {
+        App.task1();
+        task2();
+        task3();
+        task4();
+    }
 
-        // Task 2
+    private static void task1() throws IOException {
+        System.out.println("Enter the first string");
+        String firstLine = br.readLine();
 
-        // Task 3...
+        System.out.println("Enter the second string");
+        String secondLine = br.readLine();
 
+        System.out.println("Enter the third string");
+        String thirdLine = br.readLine();
+
+        System.out.println(thirdLine);
+        System.out.println(secondLine);
+        System.out.println(firstLine);
+    }
+
+    private static void task2() throws IOException {
+        System.out.println("Enter 3 numbers");
+
+        double firstNumber = Integer.parseInt(br.readLine());
+        double secondNumber = Integer.parseInt(br.readLine());
+        double thirdNumber = Integer.parseInt(br.readLine());
+        double average = ((firstNumber + secondNumber + thirdNumber) / 3);
+
+        System.out.println("The average of the numbers = " + average);
+    }
+
+    private static void task3() throws IOException {
+        System.out.println("Enter 2 numbers (a&b)");
+        double a = Double.parseDouble((br.readLine()));
+        double b = Double.parseDouble((br.readLine()));
+
+        double firstValue = (a + b);
+        double secondValue = (a - b);
+        double thirdValue = (a * b);
+
+        System.out.println("a + b = " + firstValue);
+        System.out.println("a - b = " + secondValue);
+        System.out.println("a * b = " + thirdValue);
+
+        if (b == 0) {
+            System.out.println("Error");
+        } else {
+            double fourthValue = (a / b);
+            System.out.println("a / b = " + fourthValue);
+        }
+    }
+
+    private static void task4() throws IOException {
+        System.out.println("How old are you?");
+
+        String answer = br.readLine();
+        System.out.println("You are " + answer + " years old");
     }
 }
+
+
+
