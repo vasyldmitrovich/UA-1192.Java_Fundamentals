f9845f2e8474f4e6e1b0d22d5b264b73fd1e7c2e
SaenkoVova <vova080520@gmail.com>
2024-04-30 23:12:20 +0300

    Add edu03 hw task 3

--- src/com/softserve/edu03/hw/App.java
+++ src/com/softserve/edu03/hw/App.java
@@ -24,6 +24,40 @@ public class App {
         System.out.println("The smallest number is: " + findSmallestNumber(firstNumber, secondNumber, thirdNumber));
     }
 
+    public static void task3() {
+        Person person1 = new Person();
+        person1.setFirstName("John");
+        person1.setLastName("Smith");
+        person1.setBirthYear(1990);
+
+        Person person2 = new Person("Jane", "Jackson");
+        person2.setBirthYear(1994);
+
+        Person person3 = new Person();
+        person3.input();
+
+        Person person4 = new Person("", "Johnson");
+        person4.setBirthYear(1998);
+
+        System.out.println("Add new name to person 4: ");
+        String person4Name = scanner.nextLine();
+        person4.changeName(person4Name);
+
+        Person person5 = new Person("", "");
+        System.out.println("Add new name to person 5: ");
+        String person5Name = scanner.nextLine();
+        System.out.println("Add new last name to person 5: ");
+        String person5LastName = scanner.nextLine();
+        person5.changeName(person5Name, person5LastName);
+        person2.setBirthYear(2000);
+
+        person1.output();
+        person2.output();
+        person3.output();
+        person4.output();
+        person5.output();
+    }
+
     public static double calcTriangleArea(double firstSide, double secondSide, double thirdSide) {
         double s = (firstSide * secondSide * thirdSide) / 2;
         return Math.sqrt(s * (s - firstSide) * (s - secondSide) * (s - thirdSide));
--- src/com/softserve/edu03/hw/Person.java
+++ src/com/softserve/edu03/hw/Person.java
@@ -1,7 +1,74 @@
 package com.softserve.edu03.hw;
 
+import java.time.Year;
+import java.util.Scanner;
+
 public class Person {
     private String firstName;
+
     private String lastName;
+
     private int birthYear;
+
+    public Person() {
+        this.firstName = "";
+        this.lastName = "";
+        this.birthYear = 0;
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public int getAge() {
+        return Year.now().getValue() - this.birthYear;
+    }
+
+    public void input() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input person first name: ");
+        this.firstName = sc.nextLine();
+        System.out.println("Input person last name: ");
+        this.lastName = sc.nextLine();
+        System.out.println("Input person birth year: ");
+        this.birthYear = sc.nextInt();
+    }
+
+    public void output() {
+        System.out.println("Name: " + this.firstName + " " + this.lastName + ", birth year: " + this.birthYear);
+    }
+
+    public void changeName(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public void changeName(String firstName) {
+        this.firstName = firstName;
+    }
 }
