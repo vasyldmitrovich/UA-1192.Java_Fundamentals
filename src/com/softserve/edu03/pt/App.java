ddde8609d3ee547c197898cf81dc72b1d9630f44
Rostyslav Zadyraichuk <rostykzadyraichuk010@gmail.com>
2024-05-01 12:58:43 +0300

    Practical Tasks 03-Intro to OOP

---
+++ src/com/softserve/edu03/pt/App.java
@@ -0,0 +1,48 @@
+package com.softserve.edu03.pt;
+
+import java.util.Scanner;
+
+public class App {
+
+    private static final Scanner SC = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        demonstrateNumberUtils();
+        System.out.println();
+        demonstrateEmployees();
+    }
+
+    private static void demonstrateNumberUtils() {
+        try {
+            System.out.print("Enter first number: ");
+            double firstNumber = NumberUtils.getNumber(SC.nextLine());
+            System.out.print("Enter second number: ");
+            double secondNumber = NumberUtils.getNumber(SC.nextLine());
+
+            System.out.printf("The sum of %f and %f is %f%n",
+                    firstNumber, secondNumber, NumberUtils.getTotal(firstNumber, secondNumber));
+            System.out.printf("The average of %f and %f is %f%n",
+                    firstNumber, secondNumber, NumberUtils.getAverage(firstNumber, secondNumber));
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void demonstrateEmployees() {
+        try {
+            System.out.println("Enter name, rate and hours by 1st Employee (in one line by space):");
+            Employee firstEmployee = new Employee(SC.nextLine());
+            System.out.println("Enter name, rate and hours by 2nd Employee (in one line by space):");
+            Employee secondEmployee = new Employee(SC.nextLine());
+            System.out.println("Enter name, rate and hours by 3rd Employee (in one line by space):");
+            Employee thirdEmployee = new Employee(SC.nextLine());
+
+            System.out.println(firstEmployee);
+            System.out.println(secondEmployee);
+            System.out.println(thirdEmployee);
+            System.out.printf("Total employees' salary: %f%n", Employee.getTotalSum());
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/com/softserve/edu03/pt/NumberUtils.java
@@ -0,0 +1,24 @@
+package com.softserve.edu03.pt;
+
+public class NumberUtils {
+
+    //avoid instances creation
+    private NumberUtils () {}
+
+    public static int getTotal(int first, int second) {
+        return first + second;
+    }
+    public static double getTotal(double first, double second) {
+        return first + second;
+    }
+
+    public static double getAverage(double first, double second) {
+        return (first + second) / 2;
+    }
+
+    //if user inputs few numbers - return only first
+    public static double getNumber(String prompt) throws NumberFormatException {
+        String[] parts = prompt.split(" ");
+        return Double.parseDouble(parts[0]);
+    }
+}
