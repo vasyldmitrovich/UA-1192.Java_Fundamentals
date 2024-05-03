fe62e5d0e080afbdf23cec7f721414b16a98ca4d
dimasowner <dimasowner7201@gmail.com>
2024-05-03 17:55:37 +0300

    my practical tasks 3

---
+++ src/com/softserve/edu03/pt/Calculation.java
@@ -0,0 +1,29 @@
+package com.softserve.edu03.pt;
+
+import java.util.Scanner;
+
+public class Calculation {
+    public static void main(String[] args) {
+
+        int num1 = getNumber("Enter the first number:");
+        int num2 = getNumber("Enter the second number:");
+        int total = getTotal(num1, num2);
+        double average = getAverage(num1, num2);
+
+        System.out.println("Sum of " + num1 + " and " + num2 + " is " + total);
+        System.out.println("Average of " + num1 + " and " + num2 + " is " + average);
+    }
+
+    public static int getTotal(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static double getAverage(int num1, int num2) {
+        return (double) getTotal(num1, num2) / 2;
+    }
+    private static int getNumber(String prompt) {
+        System.out.print(prompt);
+        Scanner sc = new Scanner(System.in);
+        return sc.nextInt();
+    }
+}
