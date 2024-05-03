23cb1f98ecb3a0b35b3008a97f451e53d97fe335
PylypetsAndriy <167655450+PylypetsAndriy@users.noreply.github.com>
2024-05-03 15:42:10 +0300

    myHomeWork3_OOP

---
+++ src/edu03/Person.java
@@ -0,0 +1,69 @@
+package edu03;
+
+import java.util.Scanner;
+
+import static com.softserve.edu01.hw.App.scanner;
+
+public class Person {
+    private String name;
+    private String surname;
+    private int birthYear;
+
+    public Person() {
+    }
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+
+    }
+
+    public int getAge() {
+        int currentYear = java.time.Year.now().getValue();
+        return currentYear - this.birthYear;
+    }
+
+    public void input() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        this.name = sc.nextLine();
+        System.out.print("Enter your surname: ");
+        this.surname = sc.nextLine();
+        System.out.print("Enter your birth year: ");
+        this.birthYear = sc.nextInt();
+        scanner.nextLine();
+
+    }
+
+    public void output() {
+        System.out.println("Name: " + name);
+        System.out.println("Surname: " + surname);
+        System.out.println("Birth year: " + birthYear);
+        System.out.println("age: " + getAge());
+    }
+
+    public void changeName(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+}
+    class Main{
+        public static void main(String[] args) {
+            Person[] people = new Person[5];
+
+            for (int i = 0; i < people.length; i++) {
+                people[i] = new Person();
+
+                System.out.println("input infotmation about person " + (i + 1));
+                people[i].input();
+            }
+            System.out.println("/nInformation about person");
+            for (Person person : people) {
+                person.output();
+                System.out.println();
+            }
+        }
+        }
+
+
+
