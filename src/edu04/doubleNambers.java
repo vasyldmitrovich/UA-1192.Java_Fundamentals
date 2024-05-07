67c569f8217b6a1fc06031194cccf510fba2211c
PylypetsAndriy <167655450+PylypetsAndriy@users.noreply.github.com>
2024-05-07 15:09:16 +0300

    PT3_OOP, HW4

---
+++ src/edu04/doubleNambers.java
@@ -0,0 +1,24 @@
+package edu04;
+
+import java.util.Scanner;
+
+public class doubleNambers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("input 3 fractional numbers");
+        double number1 = sc.nextDouble();
+        double number2 = sc.nextDouble();
+        double number3 = sc.nextDouble();
+
+        if(isInRange(number1) && isInRange(number2) && isInRange(number3)){
+            System.out.println("all three numbers depend to the range(-5, 5).");
+        }else {
+            System.out.println("not all three numbers depend to the range(-5, 5).");
+        }
+        sc.close();
+
+    }
+    public static boolean isInRange(double number) {
+        return number >= -5 && number <= 5;
+    }
+}
