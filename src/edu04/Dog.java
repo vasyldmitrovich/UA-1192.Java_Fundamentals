d08b3c914ac4d5870eecd86fb98dc2a8df5cbf6b
PylypetsAndriy <167655450+PylypetsAndriy@users.noreply.github.com>
2024-05-07 16:29:08 +0300

    HW5

---
+++ src/edu04/Dog.java
@@ -0,0 +1,54 @@
+package edu04;
+
+public class Dog {
+    private String name;
+    private Breed breed;
+    private int age;
+
+    public enum Breed{
+        LABRADOR, SHEPHERD, BULDOG, BEAGLE
+
+    }
+
+    public Dog(String name, Breed breed, int age) {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+    public Breed getBreed() {
+        return breed;
+    }
+    public int getAge() {
+        return age;
+    }
+    public static void main(String[] args) {
+        Dog dog1 = new Dog("Jecky", Breed.SHEPHERD, 5);
+        Dog dog2 = new Dog("Jecky", Breed.BULDOG, 7);
+        Dog dog3 = new Dog("John", Breed.BEAGLE, 2);
+
+        if (dog1.getName().equals(dog2.getName()) || dog1.getName().equals(dog3.getName()) || dog2.getName().equals(dog3.getName()))
+        {
+            System.out.println("thare is dogs with same name.");
+        } else {
+            System.out.println("thare is not dogs with same name.");
+        }
+
+        // Знаходимо найстарішу собаку
+        Dog oldestDog = dog1;
+        if (dog2.getAge() > oldestDog.getAge()) {
+            oldestDog = dog2;
+        }
+        if (dog3.getAge() > oldestDog.getAge()) {
+            oldestDog = dog3;
+        }
+
+        System.out.println("name and breed the oldest dog:");
+        System.out.println("name: " + oldestDog.getName());
+        System.out.println("breed: " + oldestDog.getBreed());
+    }
+}
+
+
