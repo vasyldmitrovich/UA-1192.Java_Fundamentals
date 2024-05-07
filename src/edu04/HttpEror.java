0fe87d068abb26a4e3a4a169e772980666a838a9
PylypetsAndriy <167655450+PylypetsAndriy@users.noreply.github.com>
2024-05-07 15:34:31 +0300

    HW3

---
+++ src/edu04/HttpEror.java
@@ -0,0 +1,49 @@
+package edu04;
+
+import java.util.Scanner;
+
+public class HttpEror {
+    enum HTTPError {
+        BadRequest(400, "Bad Request"),
+        Unauthorized(401, "Unauthorized"),
+        PaymentRequired(402, "Payment Required"),
+        Forbidden(403, "Forbidden"),
+        NotFound(404, "Not Found"),
+        MethodNotAllowed(405, "Method Not Allowed"),
+        InternalServerError(500, "Internal Server Error"),
+        NotImplemented(501, "Not Implemented"),
+        BadGateway(502, "Bad Gateway"),
+        ServiceUnavailable(503, "Service Unavailable");
+
+        private final int errorCode;
+        private final String errorName;
+
+        HTTPError(int errorCode, String errorName) {
+            this.errorCode = errorCode;
+            this.errorName = errorName;
+        }
+        public int getErrorCode() {
+            return errorCode;
+        }
+        public String getErrorName() {
+            return errorName;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("input code mistake HTTP: ");
+        int errorCode = sc.nextInt();
+
+        boolean found = false;
+        for (HTTPError error : HTTPError.values()) {
+            if (error.getErrorCode() == errorCode) {
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println("unknown code mistake HTTP: " + errorCode);
+        }
+        sc.close();
+    }
+}
