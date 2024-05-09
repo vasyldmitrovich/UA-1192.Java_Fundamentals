cac9fce986367f90df77dc843a05d288015a8ce0
SaenkoVova <vova080520@gmail.com>
2024-05-09 15:27:13 +0300

    Add edu05 hw task 2

--- src/com/softserve/edu05/hw/App.java
+++ src/com/softserve/edu05/hw/App.java
@@ -7,11 +7,33 @@ public class App {
 
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     private static void task1() {
         MonthDaysCalculator.task();
         SumProductCalculator.task();
         ResultsFinder.task();
+    }
+
+    private static void task2() {
+        do {
+
+            System.out.println("Input first number: ");
+            double firstName = scanner.nextDouble();
+
+            System.out.println("Input second number: ");
+            double secondNumber = scanner.nextDouble();
+
+            System.out.println("Sum of calculated numbers: " + (firstName + secondNumber));
+
+            System.out.println("Do you want to continue? (Y/N): ");
+            String answer = scanner.next();
+
+            if (!"Y".equals(answer)) {
+                break;
+            }
+
+        } while (true);
     }
 }
