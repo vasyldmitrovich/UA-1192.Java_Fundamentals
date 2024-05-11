f4f95714e6369f0fe5eeb2d691ced5a7285542e7
Дмитро <deputatfag@gmail.com>
2024-05-11 12:47:51 +0300

    pt05

---
+++ src/com/softserve/edu05/Employee.java
@@ -0,0 +1,12 @@
+package com.softserve.edu05;
+public class Employee {
+    String name;
+    int departmentNumber;
+    double salary;
+
+    Employee(String name, int departmentNumber, double salary) {
+        this.name = name;
+        this.departmentNumber = departmentNumber;
+        this.salary = salary;
+    }
+}
---
+++ src/com/softserve/edu05/PrimeChecker.java
@@ -0,0 +1,17 @@
+package com.softserve.edu05;
+
+public class PrimeChecker {
+    public static boolean isPrimeNumber(int number) {
+        if (number <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
