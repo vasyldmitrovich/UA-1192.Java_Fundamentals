a42e3f6c341bae67a8106e7ad0fb27178b7fa7fb
dimasowner <dimasowner7201@gmail.com>
2024-05-13 22:34:56 +0300

    edu05 pt tasks 1-4

--- src/com/softserve/edu05/hw/Car.java
+++ src/com/softserve/edu05/hw/Car.java
@@ -1,5 +1,7 @@
 package com.softserve.edu05.hw;
 
+import java.util.Objects;
+
 public class Car {
     private String type;
     private int yearOfProduction;
@@ -15,16 +17,6 @@ public class Car {
         return yearOfProduction;
     }
 
-
-    @Override
-    public String toString() {
-        return "Car{" +
-                "type='" + type + '\'' +
-                ", yearOfProduction=" + yearOfProduction +
-                ", engineCapacity=" + engineCapacity +
-                '}';
-    }
-
     static void sort(Car[] cars) {
         for (int i = 0; i < cars.length - 1; i++) {
             for (int j = 0; j < cars.length - 1 - i; j++) {
@@ -37,4 +29,22 @@ public class Car {
         }
 
     }
+    @Override
+    public String toString() {
+        return "Car{" +
+                "type='" + type + '\'' +
+                ", yearOfProduction=" + yearOfProduction +
+                ", engineCapacity=" + engineCapacity +
+                '}';
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return yearOfProduction == car.yearOfProduction && engineCapacity == car.engineCapacity && Objects.equals(type, car.type);
+    }
+
 }
