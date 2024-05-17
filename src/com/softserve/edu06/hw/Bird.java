64fedbcc676585c06dbaa98d8f09f92e4fa4ae57
PylypetsAndriy <167655450+PylypetsAndriy@users.noreply.github.com>
2024-05-17 16:56:22 +0300

    hw

---
+++ src/com/softserve/edu06/hw/Bird.java
@@ -0,0 +1,61 @@
+package com.softserve.edu06.hw;
+
+abstract class Bird {
+    String name;
+
+    public Bird(String name) {
+        this.name = name;
+    }
+
+    abstract void fly();
+}
+
+class Sparrow extends Bird {
+    public Sparrow(String name) {
+        super(name);
+    }
+
+    @Override
+    void fly() {
+        System.out.println(name + " (Sparrow) is flying short distances.");
+    }
+}
+
+class Eagle extends Bird {
+    public Eagle(String name) {
+        super(name);
+    }
+
+    @Override
+    void fly() {
+        System.out.println(name + " (Eagle) is soaring high in the sky.");
+    }
+}
+
+class Penguin extends Bird {
+    public Penguin(String name) {
+        super(name);
+    }
+
+    @Override
+    void fly() {
+        System.out.println(name + " (Penguin) cannot fly but swims very well.");
+    }
+}
+
+ class Main {
+    public static void main(String[] args) {
+        Bird[] birds = new Bird[3];
+
+        // Додавання різних типів птахів до масиву
+        birds[0] = new Sparrow("Jack");
+        birds[1] = new Eagle("Eddie");
+        birds[2] = new Penguin("Pingu");
+
+        // Виклик методу fly() для кожного птаха та виведення інформації
+        for (Bird bird : birds) {
+            System.out.println("Bird Name: " + bird.name);
+            bird.fly();
+        }
+    }
+}
