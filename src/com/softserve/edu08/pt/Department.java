83a0c3326a1a35b7ff791589e50adf801b095e5f
Pavel_Chernyavckii <Chernyavckii15@gmail.com>
2024-05-22 22:05:37 +0300

    add spaces

--- src/com/softserve/edu08/pt/Department.java
+++ src/com/softserve/edu08/pt/Department.java
@@ -1,45 +1,47 @@
 package com.softserve.edu08.pt;
 
-public class Department implements Cloneable{// Where spaces ?
+public class Department implements Cloneable {// Where spaces ?
     private String name;
     private Address address = new Address();
 
-    public Department(){
-        name="";
+    public Department() {
+        name = "";
     }
 
-    public Department(String name,String city,String street,int building){
-        this.name=name;
-        this.address.city=city;
-        this.address.street=street;
-        this.address.building=building;
+    public Department(String name, String city, String street, int building) {
+        this.name = name;
+        this.address.city = city;
+        this.address.street = street;
+        this.address.building = building;
 
     }
 
     public void setCity(String city) {
-        this.address.city=city;
+        this.address.city = city;
     }
 
-    public void showDepartment(){
-        System.out.println("Info about department "+name+
-                "\nCity:"+ address.city+
-                "\nStreet:"+ address.street+
-                "\nBuilding:"+ address.building+
+    public void showDepartment() {
+        System.out.println("Info about department " + name +
+                "\nCity:" + address.city +
+                "\nStreet:" + address.street +
+                "\nBuilding:" + address.building +
                 "\n***********************");
     }
+
     @Override
-    protected Object clone() throws CloneNotSupportedException{
+    protected Object clone() throws CloneNotSupportedException {
         Department copydep = (Department) super.clone();
         copydep.address = (Address) copydep.address.clone();
         return copydep;
     }
-    public class Address implements Cloneable{
+
+    public class Address implements Cloneable {
         private String city;
         private String street;
         private int building;
 
         @Override
-        protected Object clone()throws CloneNotSupportedException{
+        protected Object clone() throws CloneNotSupportedException {
             return super.clone();
         }
 
