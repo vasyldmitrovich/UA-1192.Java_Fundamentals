f8ef89b498d7dc3754945362b68f29c0771f7dde
SaenkoVova <vova080520@gmail.com>
2024-05-26 22:45:06 +0300

    Add edu10 hw 2,3

--- src/com/softserve/edu10/hw/App.java
+++ src/com/softserve/edu10/hw/App.java
@@ -1,12 +1,17 @@
 package com.softserve.edu10.hw;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Demonstrate task 1");
         task1();
+
+        System.out.println("Demonstrate task 2");
+        task2();
+
+        System.out.println("Demonstrate task 3");
+        task3();
     }
 
     public static void task1() {
@@ -53,5 +58,66 @@ public class App {
         Set<T> set = new HashSet<>(set1);
         set.retainAll(set2);
         return set;
+    }
+
+    public static void task2() {
+        Map<String, String> personMap = new HashMap<>();
+
+        personMap.put("Smith", "John");
+        personMap.put("Johnson", "Emily");
+        personMap.put("Williams", "Michael");
+        personMap.put("Jones", "Sarah");
+        personMap.put("Brown", "David");
+        personMap.put("Davis", "Jessica");
+        personMap.put("Miller", "Daniel");
+        personMap.put("Wilson", "Laura");
+        personMap.put("Moore", "Orest");
+        personMap.put("Taylor", "Anna");
+
+        for (Map.Entry<String, String> entry : personMap.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+
+        List<String> values = new ArrayList<>(personMap.values());
+        Set<String> set = new HashSet<>(values);
+
+        System.out.println();
+
+        if (set.size() == values.size()) {
+            System.out.println("The map doesn't contain the same values");
+        } else {
+            System.out.println("The map contains the same values");
+        }
+
+        String name = "Orest";
+
+        personMap.values().remove(name);
+
+        System.out.println("\nMap after deleting person with name " + name);
+        for (Map.Entry<String, String> entry : personMap.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+    }
+
+    public static void task3() {
+        List<Student> students = new ArrayList<>(List.of(
+                new Student("John Doe", 1),
+                new Student("Emily Johnson", 2),
+                new Student("Michael Williams", 1),
+                new Student("Sarah Jones", 3),
+                new Student("David Brown", 2)
+        ));
+
+
+        System.out.println("Students in course 1:");
+        Student.printStudents(students, 1);
+
+        students.sort(Student.compareByName());
+        System.out.println("Students sorted by name:");
+        students.forEach(System.out::println);
+
+        students.sort(Student.compareByCourse());
+        System.out.println("Students sorted by course:");
+        students.forEach(System.out::println);
     }
 }
---
+++ src/com/softserve/edu10/hw/Student.java
@@ -0,0 +1,64 @@
+package com.softserve.edu10.hw;
+
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+
+public class Student {
+
+    private String name;
+
+    private int course;
+
+    public Student() {
+        this.name = "";
+        this.course = 0;
+    }
+
+    public Student(String name, int course) {
+        this.name = name;
+        this.course = course;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public void setCourse(int course) {
+        this.course = course;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static Comparator<Student> compareByName() {
+        return Comparator.comparing(Student::getName);
+    }
+
+    public static Comparator<Student> compareByCourse() {
+        return Comparator.comparingInt(Student::getCourse);
+    }
+
+    public static void printStudents(List<Student> students, int course) {
+        Iterator<Student> iterator = students.iterator();
+        while (iterator.hasNext()) {
+            Student student = (Student) iterator.next();
+            if (student.getCourse() == course) {
+                System.out.println(student.getName() + " " + student.getCourse());
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", course=" + course +
+                '}';
+    }
+}
