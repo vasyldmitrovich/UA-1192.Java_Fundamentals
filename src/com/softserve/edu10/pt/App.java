52f98951e4cdf1e10fd8d3ad0d05005b3af1382c
SaenkoVova <vova080520@gmail.com>
2024-05-26 21:57:08 +0300

    Add edu10 pt 2

--- src/com/softserve/edu10/pt/App.java
+++ src/com/softserve/edu10/pt/App.java
@@ -1,19 +1,24 @@
 package com.softserve.edu10.pt;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class App {
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
-        System.out.println("Demonstrate task 1");
-        task1();
+//        System.out.println("Demonstrate task 1");
+//        task1();
 
+        System.out.println("Demonstrate task 2");
+        task2();
     }
 
     public static void task1() {
         Map<Integer, String> employeeMap = new HashMap<>();
+
+        Scanner scanner = new Scanner(System.in);
 
         employeeMap.put(1, "John");
         employeeMap.put(2, "Jane");
@@ -25,7 +30,6 @@ public class App {
 
         System.out.println("Enter employee id: ");
 
-        Scanner scanner = new Scanner(System.in);
         int employeeId = scanner.nextInt();
 
         if (employeeMap.containsKey(employeeId)) {
@@ -38,9 +42,108 @@ public class App {
         String name = scanner.next();
 
         if (employeeMap.containsValue(name)) {
-            System.out.println("Employee with name " + name + " has id ");
+            System.out.println("Employee with name " + name + " has id " + getKey(employeeMap, name));
         } else {
             System.out.println("Employee with name " + name + " does not exist");
         }
+    }
+
+    public static  <K, V> K getKey(Map<K, V> map, V value) {
+        for (Map.Entry<K, V> entry : map.entrySet()) {
+            if (entry.getValue().equals(value)) {
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
+
+    public static void task2() {
+        Map<Integer, Employee> employeeMap = new HashMap<>();
+        String answer;
+
+        do {
+            Employee employee = createEmployee();
+
+            if (employeeMap.containsKey(employee.getId())) {
+                System.out.println("Employee with id " + employee.getId() + " already exists");
+            } else {
+                employeeMap.put(employee.getId(), employee);
+            }
+
+            System.out.println("Do you want to add another one employee? (y/n)");
+            answer = scanner.next();
+
+        } while (!answer.equals("n"));
+
+        System.out.println("Generated employee map:");
+        for (Map.Entry<Integer, Employee> entry : employeeMap.entrySet()) {
+            System.out.println(entry.getValue());
+        }
+
+        System.out.println("Do you want to edit some employee? (y/n)");
+        answer = scanner.next();
+
+        if (answer.equals("y")) {
+            do {
+                System.out.println("Enter employee id: ");
+                int employeeId = scanner.nextInt();
+
+                if (employeeMap.containsKey(employeeId)) {
+                    employeeMap.remove(employeeId);
+                    Employee employee = createEmployee();
+
+                    employeeMap.put(employee.getId(), employee);
+                } else {
+                    System.out.println("Employee with id " + employeeId + " does not exist");
+                }
+
+
+                System.out.println("Do you want to edit another one employee? (y/n)");
+                answer = scanner.next();
+
+            } while (!answer.equals("n"));
+
+            System.out.println("Updated employee map:");
+            for (Map.Entry<Integer, Employee> entry : employeeMap.entrySet()) {
+                System.out.println(entry.getValue());
+            }
+        }
+
+        System.out.println("Select map sorting (id/name/position)");
+
+        Comparator<Employee> comparator = switch (scanner.next()) {
+            case "position" -> Comparator.comparing(Employee::getPosition);
+            case "name" -> Comparator.comparing(Employee::getName);
+            default -> Comparator.comparing(Employee::getId);
+        };
+
+        LinkedHashMap<Integer, Employee> sortedMap = employeeMap.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(comparator))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+
+
+        System.out.println("Sorted employee map:");
+        for (Map.Entry<Integer, Employee> entry : sortedMap.entrySet()) {
+            System.out.println(entry.getValue());
+        }
+    }
+
+    public static Employee createEmployee() {
+        System.out.println("Input employee id");
+        int id = scanner.nextInt();
+
+        System.out.println("Input employee name");
+        String name = scanner.next();
+
+        System.out.println("Input employee salary");
+        double salary = scanner.nextDouble();
+
+        System.out.println("Input employee position");
+        String position = scanner.next();
+
+        System.out.println("Input employee date of birth");
+        String dateOfBirth = scanner.next();
+
+        return new Employee(id, name, salary, position, dateOfBirth);
     }
 }
---
+++ src/com/softserve/edu10/pt/Employee.java
@@ -0,0 +1,81 @@
+package com.softserve.edu10.pt;
+
+public class Employee {
+
+    private int id;
+
+    private String name;
+
+    private double salary;
+
+    private String position;
+
+    private String dateOfBirth;
+
+    public Employee() {
+        this.id = 0;
+        this.name = "";
+        this.salary = 0.0;
+        this.position = "";
+        this.dateOfBirth = "";
+    }
+
+    public Employee(int id, String name, double salary, String position, String dateOfBirth) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.position = position;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", salary=" + salary +
+                ", position='" + position + '\'' +
+                ", dateOfBirth='" + dateOfBirth + '\'' +
+                '}';
+    }
+}
