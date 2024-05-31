f138dc71fe0001fee1bb82ef1002a3db3f015092
roioleksii <roy.kviten@gmail.com>
2024-05-31 10:35:22 +0300

    10 Hw + Pt

--- src/com/softserve/edu09_10_collec/hw10/App.java
+++ src/com/softserve/edu09_10_collec/hw10/App.java
@@ -1,17 +1,20 @@
 package com.softserve.edu09_10_collec.hw10;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class App {
 
     public static void main(String[] args) {
 
-task1();
+//task1();
+        task2();
 
     }
 
-    public static void task1(){
+    public static void task1() {
 
         Set<String> set1 = new HashSet<>();
         set1.add("Element 1");
@@ -28,9 +31,44 @@ task1();
 
         System.out.println("set2 =" + set2);
         System.out.println("union =" + union(set1, set2));
-        System.out.println("intersect =" + intersect(set1,  set2));
+        System.out.println("intersect =" + intersect(set1, set2));
 
     }
+
+    public static void task2() {
+
+        Map<String, String> personMap = new HashMap<>();
+        personMap.put("Roi", "Oleksii");
+        personMap.put("Lastname 1", "Name 1");
+        personMap.put("Lastname 2", "Name 2");
+        personMap.put("Lastname 2", "Name 3");
+        personMap.put("Lastname 4", "Oleksii");
+        personMap.put("Lastname 5", "Name 5");
+        personMap.put("Lastname 6", "Name 6");
+        personMap.put("Lastname 7", "Name 7");
+        personMap.put("Lastname 8", "Name 8");
+        personMap.put("Lastname 9", "Name 9");
+
+        for (Map.Entry person : personMap.entrySet()) {
+            System.out.println(person.getKey() + ": " + person.getValue());
+        }
+
+        String key= "1";
+        for (Map.Entry person : personMap.entrySet()) {
+            if (person.getValue().equals("Name 6")) {
+                key = (String) person.getKey();
+                break;
+            }
+        }
+
+        personMap.remove(key);
+
+
+        for (Map.Entry person : personMap.entrySet()) {
+            System.out.println(person.getKey() + ": " + person.getValue());
+        }
+    }
+
 
     public static <T> Set<T> union(Set<T> set1, Set<T> set2) {
         Set<T> resultSet = new HashSet<>(set1);
