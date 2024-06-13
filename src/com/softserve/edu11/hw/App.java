722a74629777b1701a9d8bb189c0420cb849bbf6
Vasyldmitrovich <vasyl.dmitrovich@gmail.com>
2024-06-13 12:55:56 +0300

    I added some comments, and you could make changes in your code using my advices

--- src/com/softserve/edu11/hw/App.java
+++ src/com/softserve/edu11/hw/App.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) {// Nice
         System.out.println("=".repeat(20) + "Task 1" + "=".repeat(20));
         task1();
         System.out.println("=".repeat(20) + "Task 2" + "=".repeat(20));
