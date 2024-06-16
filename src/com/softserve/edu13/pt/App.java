53167b1878e1383a21d8e9f32ff0e132d50c9f46
OleksandrKhomenko <o.o.khomenko.vntu@gmail.com>
2024-06-16 21:05:57 +0300

    edit edu13 pt

--- src/com/softserve/edu13/pt/App.java
+++ src/com/softserve/edu13/pt/App.java
@@ -6,12 +6,12 @@ public class App {
     public static void main(String[] args) {
         PracticalTask pt = new PracticalTask();
 
-//        System.out.println("Practical task 1");
-//        pt.task1();
-//        System.out.println("Practical task 2");
-//        pt.task2();
-//        System.out.println("Practical task 3");
-//        pt.task3();
+        System.out.println("Practical task 1");
+        pt.task1();
+        System.out.println("Practical task 2");
+        pt.task2();
+        System.out.println("Practical task 3");
+        pt.task3();
         System.out.println("Practical task 4");
         pt.task4();
     }
