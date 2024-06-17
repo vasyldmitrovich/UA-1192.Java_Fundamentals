f7345bca6bdd84e0afa24963000dc26624aa2047
Pavel_Chernyavckii <Chernyavckii15@gmail.com>
2024-06-18 02:23:58 +0300

    pt edu15

--- src/com/softserve/edu15/pt/AppPt.java
+++ src/com/softserve/edu15/pt/AppPt.java
@@ -5,16 +5,18 @@ import java.util.Scanner;
 public class AppPt {
     public static final Scanner SCANNER = new Scanner(System.in);
     public static final String TWENTYRISKS = "-".repeat(20);
+
     public static void main(String[] args) {
-        System.out.println(TWENTYRISKS + " Practical task 1 "+ TWENTYRISKS);
+        System.out.println(TWENTYRISKS + " Practical task 1 " + TWENTYRISKS);
         RepeatMsgWithInterval();
-        System.out.println(TWENTYRISKS + " Practical task 2 "+ TWENTYRISKS);
+        System.out.println(TWENTYRISKS + " Practical task 2 " + TWENTYRISKS);
         printTwoMsg();
     }
-    public static void RepeatMsgWithInterval(){
+
+    public static void RepeatMsgWithInterval() {
         System.out.println("Enter a message to repeat");
         String msg = SCANNER.nextLine();
-        for (int i = 0 ; i < 10; i++){
+        for (int i = 0; i < 10; i++) {
             System.out.println(msg);
             try {
                 Thread.sleep(1000);
@@ -23,7 +25,8 @@ public class AppPt {
             }
         }
     }
-    public static void printTwoMsg()   {
+
+    public static void printTwoMsg() {
         Thread thread1 = new MyThreadMsg("Hello, world", 1000);
         Thread thread2 = new MyThreadMsg("Peace in the world", 2000);
         thread1.start();
--- src/com/softserve/edu15/pt/MyThreadMsg.java
+++ src/com/softserve/edu15/pt/MyThreadMsg.java
@@ -1,15 +1,17 @@
 package com.softserve.edu15.pt;
 
-public class MyThreadMsg extends Thread{
+public class MyThreadMsg extends Thread {
     private String msg;
     private int interval;
-    public MyThreadMsg(String msg, int interval){
-        this.msg=msg;
-        this.interval= interval;
+
+    public MyThreadMsg(String msg, int interval) {
+        this.msg = msg;
+        this.interval = interval;
     }
+
     @Override
-    public void run(){
-        for (int i = 0; i<5 ; i++){
+    public void run() {
+        for (int i = 0; i < 5; i++) {
             System.out.println(msg);
             try {
                 sleep(interval);
