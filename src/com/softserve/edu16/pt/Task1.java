4cf031dd68de37426354250ad3e6c42eac7420d1
PylypetsAndriy <167655450+PylypetsAndriy@users.noreply.github.com>
2024-06-19 03:39:28 +0300

    pt

---
+++ src/com/softserve/edu16/pt/Task1.java
@@ -0,0 +1,35 @@
+package com.softserve.edu16.pt;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Task1 {
+    public static void main(String[] args) throws IOException {
+        int countCharacters = 0;
+        String testWord;
+        String character;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Input word:");
+        testWord = br.readLine();
+
+        System.out.println("Input character:");
+        character = br.readLine();
+
+        if (character.length() != 1) {
+            System.out.println("Please input exactly one character.");
+            return;
+        }
+
+        for (int i = 0; i < testWord.length(); i++) {
+            String currentChar = testWord.substring(i, i + 1);
+            if (currentChar.equals(character)) {
+                countCharacters++;
+            }
+        }
+
+        System.out.println("There are character '" + character + "' " + countCharacters + " times in the word '" + testWord + "'.");
+    }
+}
